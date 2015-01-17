0288d9a9adffef46ce01e9fda6cb019c1e2b1d32
Aaron Dancer <me@aarondancer.com>
2015-01-17 10:01:36 -0500

    Added accelerometer listening

--- Pengcopter.py
+++ Pengcopter.py
@@ -9,9 +9,13 @@ except liblo.ServerError, err:
     sys.exit()
 
 def concentration_callback(path, args):
-    	print (sum(args) / float(len(args)))
+    print (sum(args) / float(len(args)))
+
+def acc_callback(path, args):
+	print (sum(args) / float(len(args)))
 
 server.add_method("/muse/elements/experimental/concentration", 'f', concentration_callback)
+server.add_method("/muse/acc", 'fff', acc_callback)
 
 # loop and dispatch messages every 100ms
 while True:
