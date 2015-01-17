168f094192e74b51cb2a553e083e900fc6d3065a
Aaron Dancer <me@aarondancer.com>
2015-01-17 09:13:18 -0500

    Init commit

---
+++ Pengcopter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import liblo, sys
+
+try:
+    server = liblo.Server(5001)
+except liblo.ServerError, err:
+    print str(err)
+    sys.exit()
+
+def concentration_callback(path, args):
+    	print (sum(args) / float(len(args)))
+
+server.add_method("/muse/elements/experimental/concentration", 'f', concentration_callback)
+
+# loop and dispatch messages every 100ms
+while True:
+    server.recv(1000)
