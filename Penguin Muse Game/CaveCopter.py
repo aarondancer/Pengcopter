8acc571e35999f2592f1696bec83d27a0c29f46b
zhengt66 <zhtian@seas.upenn.edu>
2015-01-17 12:34:06 -0500

    Removed penguin's rockets

--- Penguin Muse Game/CaveCopter.py
+++ Penguin Muse Game/CaveCopter.py
@@ -444,7 +444,7 @@ def addText(text, background, xpos, ypos, \
 # Update game information on top of the screen
 def updateCopterInfo(background, state):
 
-    addText("Energy: " + str(state.copterFuel), background, 15, 3, THECOLORS['lightgrey'], (0,0,0), 20)
+    addText("Health: " + str(state.copterFuel), background, 15, 3, THECOLORS['lightgrey'], (0,0,0), 20)
     addText("Sector: " + str(state.sector), background, 210, 3, THECOLORS[state.sectorColor], (0,0,0), 20)
     addText("Score: " + str(state.copterScore), background, 440, 3, THECOLORS['cyan'], (0,0,0), 20)
     #addText("Rockets: " + str(state.copterRockets), background, 680, 3, THECOLORS['lightgreen'], (0,0,0), 20)
@@ -1069,7 +1069,7 @@ def doMainLoop(screen,background, tile):
                 if event.key == 276: # left
                     copter.xmove=-copter.xdelta
                 #if event.key == 32: # space
-                    fireRocket(rocketGroup, copter, state)
+                    #fireRocket(rocketGroup, copter, state)
                 if event.key == K_ESCAPE: # esc
                     quit();
             elif event.type == KEYUP:
